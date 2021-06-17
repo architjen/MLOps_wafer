82010504480672fd199d6c88cc3ce991dc95d252
Archit Jain <aj@AJ-Mac-Pro.home>
2021-06-18 00:22:30 +0200

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a toy project for end-to-end ML with MLOps',
+    author='archit jain',
+    license='MIT',
+)
